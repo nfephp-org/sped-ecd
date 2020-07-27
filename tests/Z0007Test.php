e24dad691a8f53600cabcd33a91c4c8af7434a9c
robmachado <linux.rlm@gmail.com>
2020-07-27 17:27:34 -0300

    adicionado teste unitario para o elemneto Z0007

---
+++ tests/Z0007Test.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace NFePHP\ECD\Tests;
+
+use NFePHP\ECD\Elements\Z0007;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+
+class Z0007Test extends TestCase
+{
+    public function testZ0007()
+    {
+        $std = new stdClass();
+        $std->COD_ENT_REF = '01';
+        $std->COD_INSCR = 'Z1234567';
+        
+        $b7 = new Z0007($std);
+        $result = "{$b7}";
+        
+        $expected = '|0007|01|Z1234567|';
+        $this->assertEquals($expected, $result);
+    }
+}
